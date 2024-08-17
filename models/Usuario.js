0d13ae3d0e9af66a42610cf9d3e8cb459dd5e3e4
acabreramata <acabreram@ucenfotec.ac.cr>
2024-08-16 18:18:23 -0600

    Controlador del login, auth.js y servicio de autenticacion + small details

---
+++ models/Usuario.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    nombre: { type: String, required: true },
+    apellido: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    idType: { type: String, required: true },
+    id: { type: String, required: true, unique: true },
+    birthdate: { type: Date, required: true }, // Tipo Date para almacenar la fecha de nacimiento
+    password: { type: String, required: true } // Esto será generado automáticamente
+}, {
+    timestamps: true,
+});
+
+module.exports = mongoose.model('User', UserSchema);
